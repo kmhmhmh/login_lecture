f5b748669a113125fa02835448c70de5f0f6aed7
김민호 <gimminho@gimminhoui-MacBookAir.local>
2023-12-25 12:15:08 +0900

    프런트 자바스크립트 만들기

--- app/app.js
+++ app/app.js
@@ -1,31 +1,14 @@
-// //http://34.66.134.216:8080/login
+const express= require("express");
+const app = express();
 
-// const express= require("express");
-// const app = express();
 
-// app.get('/', (req,res)=>{ //파라미터 요청 데이터
-//     res.send("여기는 루트입니다!");
-// }) //경로 설정
 
-// app.get('/login', (req,res)=>{
-//     res.send("여기는 로그인 화면 입니다!");
-// })
+const home = require("./src/routes/home" )//라우팅
 
-// app.listen(3000, ()=>{    //서버 넘버
-//     console.log("서버가동");//서버구동
-// });
+app.set("views", "./src/views");
+app.set("view engine", "ejs"); //뷰세팅 값
+app.use(express.static(`${__dirname}/src/publit`));
 
-// const http = require("http");
-// const app = http.createServer((req,res)=>{
-//     res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
-//     if(req.url==='/'){
-//         res.end('여기는 루트 입니다!')
-//     }else if(req.url === "/login"){
-//         res.end('여기는 로그인 화면입니다!')   
-//     }
-// });
+app.use("/", home);//use => 미들웨어를 등록해주는 메서드
 
-// app.listen(3001,()=>{
-//     console.log("http로 가동된 서버입니다!")
-// });
-
+module.exports = app
--- app/bin/www.js
+++ app/bin/www.js
@@ -1,4 +1,4 @@
-const app =require("../app_routes")
+const app =require("../app")
 const PORT = 3000;
 
 app.listen(PORT, ()=>{   
